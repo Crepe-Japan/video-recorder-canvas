3c13852925ede516b3d055fc56a57bfa72c7cd97
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2022-09-05 14:58:43 +0900

    temp: hide recording

--- capture.js
+++ capture.js
@@ -16,7 +16,7 @@ startButton.addEventListener("click", () => {
         downloadButton.href = stream;
         preview.captureStream = preview.captureStream || preview.mozCaptureStream;
         return new Promise((resolve) => preview.onplaying = resolve);
-    }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
+    })/* .then(() => startRecording(preview.captureStream(), recordingTimeMS))
         .then((recordedChunks) => {
             let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
             recording.src = URL.createObjectURL(recordedBlob);
@@ -31,7 +31,7 @@ startButton.addEventListener("click", () => {
             } else {
                 log(error);
             }
-        });
+        }); */
 }, false);
 
 stopButton.addEventListener("click", () => {
